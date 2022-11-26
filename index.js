1ce68fe5523ca418ce12dd00d87557338002eb76
vivianne <viviannjoroge91@gmail.com>
2022-11-27 00:44:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(string) {
+    let name = [];
+    for (let i = 0; i < string.length; i++) { 
+        let name1 = `Thank you,${string[i]},for the wonderful surprise gift!`;
+        name.push(name1)
+    }
+    return name;
+    }
+function countDown (number){
+    while (number >= 0){
+        console.log(number);
+        number --;
+    }
+}
